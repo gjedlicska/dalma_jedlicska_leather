f79225a6493f80a760f13b22310a1cf7d611d5da
Gergő Jedlicska <gergo@jedlicska.com>
2022-12-18 14:54:03 +0100

    initial app scaffolding

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 Portfolio and webshop for Dalma Jedlicska Leather
 
+# tailwindcss
+
+The css is using tailwind css. To compile it to the distributable css, we are using
+the tailwindcss standalona cli. 
+Get it from [here](https://github.com/tailwindlabs/tailwindcss/releases/).
--- app/main.py
+++ app/main.py
@@ -1,8 +1,28 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.gzip import GZipMiddleware
+
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.add_middleware(GZipMiddleware)
+
+print("foo")
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request) -> HTMLResponse:
+    context = {"request": request}
+    response = templates.TemplateResponse("home.html", context)
+    return response
+
+
+@app.get("/collections", response_class=HTMLResponse)
+async def collections(request: Request) -> HTMLResponse:
+    context = {"request": request}
+    response = templates.TemplateResponse("collections.html", context)
+    return response
