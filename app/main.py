fee2ed24dea9c65c3818fa39d0bfc46d74a1760b
Gergő Jedlicska <gergo@jedlicska.com>
2023-01-07 18:39:07 +0100

    add i18n selector

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.middleware import Middleware
 from fastapi.middleware.gzip import GZipMiddleware
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from starlette_babel import LocaleMiddleware, get_translator
@@ -19,6 +19,10 @@ shared_translator.load_from_directories(
     [root_dir.joinpath("locales")]
 )  # one or multiple locale directories
 
+
+global_context = {
+    "supported_locales": supported_locales
+}
 
 app = FastAPI(
     middleware=[
@@ -37,6 +41,7 @@ app.add_middleware(GZipMiddleware)
 async def home(request: Request) -> HTMLResponse:
     slideshow_images = services.get_slideshow_images()
     context = {
+        **global_context,
         "request": request,
         "logo_active": True,
         "slideshow_images": slideshow_images,
@@ -47,27 +52,38 @@ async def home(request: Request) -> HTMLResponse:
 
 @app.get("/products", response_class=HTMLResponse)
 async def products(request: Request) -> HTMLResponse:
-    context = {"request": request}
+    context = {"request": request, **global_context}
     response = templates.TemplateResponse("collections.html", context)
     return response
 
 
 @app.get("/collections", response_class=HTMLResponse)
 async def collections(request: Request) -> HTMLResponse:
-    context = {"request": request}
+    context = {"request": request, **global_context}
     response = templates.TemplateResponse("collections.html", context)
     return response
 
 
 @app.get("/stories", response_class=HTMLResponse)
 async def stories(request: Request) -> HTMLResponse:
-    context = {"request": request}
+    context = {"request": request, **global_context}
     response = templates.TemplateResponse("collections.html", context)
     return response
 
 
 @app.get("/info", response_class=HTMLResponse)
 async def info(request: Request) -> HTMLResponse:
-    context = {"request": request}
+    context = {"request": request, **global_context}
     response = templates.TemplateResponse("collections.html", context)
     return response
+
+
+@app.get("/set_lang")
+async def set_language(request: Request, lang: str) -> RedirectResponse:
+    # fallback to home if referer is not set for some reason
+    referer = request.headers.get("referer") or request.base_url
+    response = RedirectResponse(url=referer)
+    response.set_cookie("language", lang)
+    return response
+
+print(app.middleware_stack)
