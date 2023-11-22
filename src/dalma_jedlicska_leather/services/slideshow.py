85453a8efa0b9d49e4d780660ac744e180c4cde6
Gergő Jedlicska <gergo@jedlicska.com>
2023-11-22 06:57:44 +0100

    add low res images to slideshow images

--- src/dalma_jedlicska_leather/services/slideshow.py
+++ src/dalma_jedlicska_leather/services/slideshow.py
@@ -6,6 +6,7 @@ from dalma_jedlicska_leather.services.models import ImageData, ImageLink
 
 @define
 class HomeImageData(ImageData):
+    low_res_source: str
     height_sixth: int
     z_index: int
     ratio: str
@@ -17,30 +18,34 @@ def get_slideshow_images() -> Sequence[HomeImageData]:
         HomeImageData(
             "/static/images/jedlicska_dalma_film_look_brown-59.webp",
             "foobar",
+            low_res_source="/static/images/jedlicska_dalma_film_look_brown-59_blur.webp",
             height_sixth=6,
             z_index=0,
-            ratio="3/2",
+            ratio="3 / 2",
             image_link=ImageLink("/", "home", 45, 66),
         ),
         HomeImageData(
             "/static/images/jedlicska_dalma_film_look_porcelain-18.webp",
             "DUMMY THINGHY",
+            "/static/images/jedlicska_dalma_film_look_porcelain-18_blur.webp",
             4,
             2,
-            ratio="2/3",
+            ratio="2 / 3",
         ),
         HomeImageData(
             "/static/images/jedlicska_dalma_film_look_porcelain-26.webp",
             "DUMMY THINGHY",
+            "/static/images/jedlicska_dalma_film_look_porcelain-26_blur.webp",
             5,
             1,
-            ratio="1/1",
+            ratio="1 / 1",
         ),
         HomeImageData(
             "/static/images/jedlicska_dalma_collection -140.webp",
             "DUMMY THINGHY",
+            "/static/images/jedlicska_dalma_collection -140_blur.webp",
             6,
             0,
-            "3/2",
+            ratio="1 / 1",
         ),
     ]
